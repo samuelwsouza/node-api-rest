2e814d0f96c14a9c1490853b24719682301790e3
Samuel Souza <samukawsouza@gmail.com>
2026-09-08 23:26:46 -0300

    feat: transactions routes like create, list and see the transaction and summary

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,30 @@ import { randomUUID } from 'node:crypto'
 import { knex } from "../database"
 
 export async function transactionRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { transaction }
+  })
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions').sum('amount', { as: 'amount' }).first()
+
+    return { summary }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
@@ -20,5 +44,5 @@ export async function transactionRoutes(app: FastifyInstance) {
     })
 
     return reply.status(201).send()
- })
+  })
 }
